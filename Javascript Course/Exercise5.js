25f21b97535e79468f86720039ed6d477cc75bf5
JM <33376872+jmaytona@users.noreply.github.com>
2018-01-11 11:39:32 +0800

    Exercise 5 Loops and Statement

---
+++ Javascript Course/Exercise5.js
@@ -0,0 +1,28 @@
+for (x = 0; x <= 15; x++){
+    if (x % 2 == 0) {
+        console.log(x + " is an even number");
+    }
+    else {
+        console.log(x + " is an odd number");
+    }
+}
+
+/*for (x = "*"; x <= "*****"; x = x + "*"){
+        console.log(x);
+    }*/
+
+/*for (x = 1; x <= 100; x++){
+   
+    if ((x % 3 ==0)&&(x % 5 ==0)) {    
+        console.log("CodeMonkey");
+    }
+    else if (x % 5 == 0) {    
+        console.log("Monkey");
+    }
+    else if  (x % 3 == 0) {
+        console.log("Code");
+    }
+    else{
+     console.log(x);
+    }
+}*/
