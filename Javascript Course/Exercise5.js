43f6b064514e8fcba021f5698f410641d934b09d
JM <33376872+jmaytona@users.noreply.github.com>
2018-01-10 19:44:39 -0800

    2nd Problem Solved

--- Javascript Course/Exercise5.js
+++ Javascript Course/Exercise5.js
@@ -1,15 +1,15 @@
-for (x = 0; x <= 15; x++){
+/*for (x = 0; x <= 15; x++){
     if (x % 2 == 0) {
         console.log(x + " is an even number");
     }
     else {
         console.log(x + " is an odd number");
     }
-}
+}*/
 
-/*for (x = "*"; x <= "*****"; x = x + "*"){
+for (x = "*"; x <= "*****"; x = x + "*"){
         console.log(x);
-    }*/
+    }
 
 /*for (x = 1; x <= 100; x++){
    
