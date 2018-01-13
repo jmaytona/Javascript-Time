da7d9f6e0fe2b42f28030cad059d20808fa28ecd
JM <33376872+jmaytona@users.noreply.github.com>
2018-01-13 05:34:27 -0800

    Triangle Area Function

--- Javascript Course/Excercise6.js
+++ Javascript Course/Excercise6.js
@@ -1,15 +1,15 @@
-/*function triangleArea(base, height) {
+function triangleArea(base, height) {
    return base * height / 2;
 }
 
 var traingle1 = triangleArea(5, 6);
 
-console.log(traingle1 + " is the triangle area");*/
+console.log(traingle1 + " is the triangle area");
 
-function circleArea(radius) {
+/*function circleArea(radius) {
     return (radius * radius) * Math.PI ;
 }
 
 var circle1 = circleArea(50);
 
-console.log(circle1);
+console.log(circle1);*/
