e8f6d7cd0d7b8e8e92ab0c09f5b2b9d3f2d911f0
JM <33376872+jmaytona@users.noreply.github.com>
2018-01-13 07:45:34 -0800

    Update Excercise6.js

--- Javascript Course/Excercise6.js
+++ Javascript Course/Excercise6.js
@@ -1,10 +1,10 @@
-function triangleArea(base, height) {
+/*function triangleArea(base, height) {
    return base * height / 2;
 }
 
 var traingle1 = triangleArea(5, 6);
 
-console.log(traingle1 + " is the triangle area");
+console.log(traingle1 + " is the triangle area");*/
 
 /*function circleArea(radius) {
     return (radius * radius) * Math.PI ;
@@ -13,3 +13,39 @@ console.log(traingle1 + " is the triangle area");
 var circle1 = circleArea(50);
 
 console.log(circle1);*/
+
+/*function shapeArea(base, height, width, radius) {
+    if (base >= 1 && height >=1 && width ==0 && radius ==0) {
+        return base * height / 2 + " This is the area of a triangle";
+    }
+    else if (base ==0 && height ==0 && width ===0 && radius >=1) {
+        return (radius * radius) * Math.PI + " This is the area of a cricle";
+    }
+    else if (base ==0 && height >=1 && width >=1 && radius ==0) {
+        return height * width + " This is the area of a rectangle";
+    }
+    else {
+        return "Shape Undefine!"
+    }
+}
+var ex1 = shapeArea(0,0,0,0);
+console.log(ex1); */
+
+function shapeArea(shape, x, y){
+    if (shape == "triangle"){
+        return x * y /2 + " is the area of this Triangle";
+    }
+    else if (shape == "circle") {
+        return (x * x) * Math.PI + " is the area of this Circle";
+    }
+    else if (shape == "rectangle") {
+        return x * y + " is the area of this Rectangle";
+    }
+    else {
+        return "Undefine Shape";
+    }
+}
+console.log(shapeArea("triangle",5,6));
+console.log(shapeArea("circle",5));
+console.log(shapeArea("square",5,6));
+console.log(shapeArea("rectangle",9,9));
