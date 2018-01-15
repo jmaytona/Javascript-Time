517681976f70e4526c25f5bfb222d23e0e3ef188
JM <33376872+jmaytona@users.noreply.github.com>
2018-01-15 20:56:08 +0800

    Add files via upload

---
+++ Percentage Calculator/calc.js
@@ -0,0 +1,11 @@
+var textField1 = document.getElementById("textField1");
+var textField2 = document.getElementById("textField2");
+var resultField = document.getElementById("resultField");
+var form = document.getElementById("formOperation");
+
+/*textField1.value = "Eat This Shit!";
+textField2.value = "Eat This Mother Shit!";
+resultField.innerText = "Change is comming!";*/
+
+form.addEventListener("submit", function() {
+})
